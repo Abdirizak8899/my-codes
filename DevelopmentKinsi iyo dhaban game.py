a6d196baab68b04a3dccdd922de2f1376d5aea3d
Abdirizak8899 <102373033+Abdirizak8899@users.noreply.github.com>
2022-03-25 18:08:28 +0300

    Ugdsas
    Bnkoufd

---
+++ DevelopmentKinsi iyo dhaban game.py
@@ -0,0 +1,49 @@
+# developing Kinsi iyo dhaban Game!
+# registration & intro for the Game
+def registintro():
+    print("Kinsi iyo dhaban Game!")
+    
+    print("REGISTRATION")
+    Name = input("Enter your name: ")
+    Age = input("Enter your age: ")
+    print("REGISTRATION SUCCESS!")
+    
+    print("INTRO TO THE GAME!")
+    
+    print('Hello '+Name, 'welcome to the Kinsi iyo dhaban game! i hope you to enjoy the game!')
+    pass
+# copyright of the game
+def copyright():
+    print("copyright of this game has Eg.Abdirizak abdullahi hussein\n ")
+    pass
+registintro()
+copyright()
+
+# Dhaban iyo kinsi game
+print("KULANKA 1AAD")
+print('kinsi = 1  , dhaban = 2 , wah = 0')
+Dhaban = '2'
+sheeg1 = input('kinsi / dhaban / ama wah ?')
+if sheeg1 == Dhaban:
+    print("waa sheegtay baraawo")
+else:
+    print(" waaa sheegi weysay")
+    
+print("KULANKA 2AAD")
+print('kinsi = 1  , dhaban = 2 , wah = 0')
+wah = '0'
+sheeg2 = input('kinsi / dhaban / ama wah ?')
+if sheeg2 == wah:
+    print("waa sheegtay baraawo")
+else:
+    print(" waaa sheegi weysay")
+    
+print("KULANKA 3AAD")
+print('kinsi = 1  , dhaban = 2 , wah = 0')
+kinsi = '1'
+sheeg3 = input('kinsi / dhaban / ama wah ?')
+if sheeg3 == kinsi:
+    print("waa sheegtay baraawo")
+else:
+    print(" waaa sheegi weysay")
+    
